27182c435f142ccdf0863be6ea56d0934a09df1b
Yuci Gou <yuci.gou@gmail.com>
2018-11-28 10:09:51 +0000

    Add one test case

--- __tests__/one-to-many.js
+++ __tests__/one-to-many.js
@@ -11,4 +11,11 @@ describe('Objection_Ops_one-to-many_01', () => {
 		await sleep(1000)
 		expect(users[0].id).toBe('6c1adf5b-b921-4c87-9d68-096c9cfca61c')
 	})
+
+	test("Objection_Ops_one-to-many_01_querying_a_user_by_username", async () => {
+		let users = await User.query().join('identity', 'identity.user_id', 'users.id').where('identity.username', 'ygou').eager('identity')
+		console.log("Object returned: ", users[0].identity)
+		await sleep(1000)
+		expect(users[0].identity[0].username).toBe('ygou')
+	})
 })
